9d0968d3b856508ae69ed69f888e215befc956b8
Alaicev <altarev123456@mail.ru>
2023-01-30 16:24:25 +0400

    feat: add price list

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ form.addEventListener("submit", function (e) {
   let formData = new FormData(form)
 
   const priceline = [25, 30, 35]
-  const priceWidth = [710, 750, 1100]
+  const priceWidth = [[710, 750, 1100],[4300, 5600, 8400],[16000, 16000, 16000]]
 
   let width =  Number(formData.get('width'))
   let height = Number(formData.get('height'))
   let line = Number(formData.get('line')) || 0.00000000001
   let abres = Number(formData.get('abres')) || 0.00000000001
   let select = Number(formData.get('select')) 
+  let material = Number(formData.get('material')) 
   if(!width || !height || !line) {
     res.innerHTML= "Где то ошибка!!!"
     res.style.backgroundColor = "red"
   } else {
-    let result =Math.floor(priceWidth[select] / 2.25 * (width * height) + line * priceline[select] + abres * 15)
+    let result =Math.floor(priceWidth[material][select] / 2.25 * (width * height) + line * priceline[select] + abres * 15)
   res.innerHTML =`${result} р.`
   res.style.backgroundColor = "green"
   form.reset()
